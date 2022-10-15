0fac4d0d268a9755cb8c96dbaa12b351105d669b
draeder <draeder@gmail.com>
2022-10-15 02:26:12 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ from importmodules import ImportModules
 imports = ImportModules()
 
 my_classes = imports.imp('apiAdapters')
-print(my_classes)
-print(list(my_classes.values())[0])
 
-list(my_classes.values())[0]()
+list(my_classes.values())[0]()  # <-- Executes the first function of the class
